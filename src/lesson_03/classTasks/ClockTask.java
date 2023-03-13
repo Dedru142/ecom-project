931cd3c1b0a8df3e2c60fa0944e034b772a9e88f
Dedru142 <dandan999111@gmail.com>
2023-03-13 19:12:17 +0200

    Update ClockTask.java

--- src/lesson_03/classTasks/ClockTask.java
+++ src/lesson_03/classTasks/ClockTask.java
@@ -1,2 +1,23 @@
-package lesson_03.classTasks;public class ClockTask {
+package lesson_03.classTasks;
+
+import java.util.Scanner;
+public class ClockTask {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter time (hours and minutes)");
+        int time = input.nextInt();
+
+
+
+        System.out.print((time / 100) + " : ");
+        System.out.print((time % 100));
+
+
+
+
+
+
+    }
 }
