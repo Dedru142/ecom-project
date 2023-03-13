e1940907a9f4d7fe5a1ec97963f33650c6ca1b34
Dedru142 <dandan999111@gmail.com>
2023-03-13 20:18:40 +0200

    Create AssignmentTask.java

---
+++ src/lesson_03/classTasks/AssignmentTask.java
@@ -0,0 +1,16 @@
+package lesson_03.classTasks;
+
+public class AssignmentTask {
+    public static void main(String[] args) {
+
+        int a = 10;
+        a += 90;
+        a -= 25;
+        a /= 3;
+        a %= 10;
+        a *= (-3);
+        a++;
+        a--;
+        System.out.println("a = " + a);
+    }
+}
