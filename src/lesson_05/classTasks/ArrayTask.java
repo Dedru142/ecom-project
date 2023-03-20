d65cd740a487e3534a5cd3b1bc891807a845a50e
Dedru142 <dandan999111@gmail.com>
2023-03-20 18:51:49 +0200

    Added summary and average to ArrayTask

--- src/lesson_05/classTasks/ArrayTask.java
+++ src/lesson_05/classTasks/ArrayTask.java
@@ -7,18 +7,24 @@ public class ArrayTask {
 
         Scanner scanner = new Scanner(System.in);
         int numberArray[] = new int[3];
+        double summary = 0;
         System.out.println("Please enter the first number");
         numberArray[0] = scanner.nextInt();
+        summary += numberArray[0];
         System.out.println("Please enter the second number");
         numberArray[1] = scanner.nextInt();
+        summary += numberArray[1];
         System.out.println("Please enter the third number");
         numberArray[2] = scanner.nextInt();
+        summary += numberArray[2];
 
         int biggestNumber = (numberArray[0] > numberArray[1] ) ? numberArray[0] : numberArray[1];
 
         biggestNumber = (biggestNumber > numberArray[2]) ? biggestNumber : numberArray[2];
 
         System.out.println("Biggest Number = " + biggestNumber);
+        System.out.println("summary = " + summary);
+        System.out.println("Average (summary / 3.0) = " + summary / 3.0);
 
     }
 }
